475131d1005961837da501f035c80dedae81c593
Kenneth Ozdowy <koinkers@localhost.localdomain>
2015-01-08 12:58:02 -0500

    added db route for index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var db = req.db;
+  var collection = db.get('posts');
+  collection.find({},{}, function(e,docs){
+    res.render('index', {
+      "posts" : docs,
+      title: "Wolverine Blog"
+    });
+  });
 });
 
 module.exports = router;
